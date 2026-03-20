44db1a6aede49d9426630f62a92fce8199e1cb81
TWittstadt <twittstadt@artiso.com>
2026-03-20 10:23:42 +0100

    Changed directory name

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 shopt -s nullglob
 
 # Copy dotfiles to home directory
-cp -r "$SCRIPT_DIR/_home"/{.,}* ~/
+cp -r "$SCRIPT_DIR/home"/{.,}* ~/
 
 exec bash
